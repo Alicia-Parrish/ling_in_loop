9bbee3270477d892b2d0c84ca37d65e32e825851
wh629 <60375376+wh629@users.noreply.github.com>
2020-10-31 16:23:22 -0400

    include percent examples with pmi

--- scripts/analysis/corpus_stats.py
+++ scripts/analysis/corpus_stats.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import json
 import re
+import string
 import numpy as np
 
 def normalize_text(s):
@@ -17,8 +18,7 @@ def normalize_text(s):
         return ''.join(ch for ch in text if ch not in exclude)
     def lower(text):
         return text.lower()
-    #return white_space_fix(remove_articles(remove_punc(lower(s))))
-    return white_space_fix(remove_punc(lower(s)))
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
 
 
 def get_tokens(s):
@@ -26,19 +26,26 @@ def get_tokens(s):
     return normalize_text(s).split()
 
 
-def get_pmi(exs, smooth):
+def get_ex_tokens(ex, hyp_only=True):
+    if hyp_only:
+        return get_tokens(ex['hypothesis'])
+    else:
+        return get_tokens(ex['premise'] + ' ' + ex['hypothesis'])
+
+
+def get_pmi(exs, smooth, alpha, weight):
     total_docs = len(exs)
 
     counts_by_label = {
-        'entailment': {},
-        'neutral': {},
-        'contradiction': {},
+        'entailment': {'count': 0., 'word_count': {}},
+        'neutral': {'count': 0., 'word_count': {}},
+        'contradiction': {'count': 0., 'word_count': {}},
     }
 
     word_counts = {}
 
     for ex in exs:
-        toks = get_tokens(ex['hypothesis'])
+        toks = get_ex_tokens(ex, hyp_only=False)
         counts_by_label[ex['label']]['count'] += 1
         counter = counts_by_label[ex['label']]['word_count']
 
@@ -46,31 +53,36 @@ def get_pmi(exs, smooth):
             if counter.get(tok, False):
                 counter[tok] += 1
             else:
-                counter[tok] = 1
+                counter[tok] = 1.
 
             if word_counts.get(tok, False):
                 word_counts[tok] += 1
             else:
-                word_counts[tok] = 1
+                word_counts[tok] = 1.
 
-    exp_pmi = {label: {} for label in counts_by_label.keys()}
-
+    count_stats = {label: {} for label in counts_by_label.keys()}
+    vocab_size = len(list(word_counts.keys()))
     for label, count_dict in counts_by_label.items():
         label_count = count_dict['count']
         for word, word_count in word_counts.items():
-            count_w_c = counts_by_label[label].get(word, 0)
-            exp_pmi[label][word] = (
-                    (count_w_c*total_docs + smooth)/(word_count*label_count + smooth*total_docs)
-            )
+            count_w_c = counts_by_label[label]['word_count'].get(word, 0.)
 
-    pmi = {
-        label: pd.Series(np.log(pd.Series(exp_pmi[label])), index=exp_pmi[label].keys()).sort_values(
-            ascending=False,
-            inplace=True
-        ) for label in exp_pmi.keys()
+            if weight:
+                w_pmi = np.log(count_w_c*(total_docs**alpha)/(word_count**alpha)/label_count)
+            else:
+                w_pmi = np.log((count_w_c+smooth)*(total_docs+smooth*vocab_size*3)/(word_count+smooth*3)/(label_count+smooth*vocab_size))
+
+            count_stats[label][word] = {
+                'pmi': w_pmi,
+                'pcent': count_w_c/label_count,
+            }
+
+    pmi_stats = {
+        label: pd.DataFrame.from_dict(count_stats[label], orient='index').sort_values(by='pmi', ascending=False)
+        for label in count_stats.keys()
     }
 
-    return pmi
+    return pmi_stats
 
 
 def get_hyp_lengths(exs):
@@ -81,12 +93,12 @@ def get_hyp_lengths(exs):
     }
 
     for ex in exs:
-        toks = get_tokens(ex['hypothesis'])
+        toks = get_ex_tokens(ex)
         hyp_lengths[ex['label']].append(len(toks))
 
     hyp_stats = {label: {} for label in hyp_lengths.keys()}
 
-    for label, lengths in hyp_lengths:
+    for label, lengths in hyp_lengths.items():
         l_series = pd.Series(lengths)
         hyp_stats[label]['mean'] = l_series.mean()
         hyp_stats[label]['median'] = l_series.median()
@@ -109,10 +121,9 @@ def get_stats(args):
         task_name = os.path.basename(os.path.dirname(args.fname))
 
         args.out_dir = os.path.join(stats, task_name)
+    os.makedirs(args.out_dir, exist_ok=True)
 
-    print(args.out_dir)
-
-    pmis = get_pmi(ex, args.smooth)
+    pmis = get_pmi(ex, args.smooth, args.alpha, args.weight)
     hyp_lengths, hyp_stats = get_hyp_lengths(ex)
 
     pd.DataFrame(hyp_stats).to_csv(os.path.join(args.out_dir, 'hyp_length_stats.csv'))
@@ -132,6 +143,8 @@ if __name__ == '__main__':
 
     # Optional arguments(
     parser.add_argument('--smooth', help='amount to smooth pmi', default=100)
+    parser.add_argument('--alpha', help='weighting for rare words', default=0.75)
+    parser.add_argument('--weight', help='whether to smooth by adding or weighting', action='store_true')
     parser.add_argument('--out_dir', help='where to output summary stats', default='')
 
     args = parser.parse_args()
