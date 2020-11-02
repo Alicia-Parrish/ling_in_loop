be254b1b1e83c3974b61ed7d7713a49f9bf98ad9
Alicia Parrish <alicia.v.parrish@gmail.com>
2020-11-02 11:49:05 -0500

    Create link_anonid_with_displayname.R

---
+++ scripts/for_mturk/link_anonid_with_displayname.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+setwd("C:/Users/NYUCM Loaner Access/Documents/GitHub/ling_in_loop/scripts")
+
+anon_codes = read.csv("../../SECRET/ling_in_loop_SECRET/anonymized_id_links.csv")
+
+LitL_files = list.files("../../SECRET/ling_in_loop_SECRET/raw_mturk_files/Group3_ling_in_loop/Intro",full.names=T)
+
+LitL=NULL
+for(i in 1:length(LitL_files)){
+  temp = read.csv(LitL_files[i])
+  LitL = rbind(LitL,temp)
+}
+
+slack_names<-LitL%>%
+  select(WorkerId,Answer.slack_name)%>%
+  rename("Slack_name"=Answer.slack_name)
+
+anon_slack<-merge(slack_names,anon_codes,by="WorkerId")
+
+anon_slack2<-anon_slack %>% select(Slack_name,AnonId)
+
+write.csv(anon_slack2,"files/worker_data/anon_slack_names.csv")
