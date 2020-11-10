40ddaeebc4c39e9be98574cbba3fdf8779673128
woollysocks <nikitanangia@gmail.com>
2020-11-09 19:00:47 -0500

    script to set max hits per worker. commented out infinite for loop right now

--- scripts/max_hits_per_worker.sh
+++ scripts/max_hits_per_worker.sh
@@ -1,41 +1,37 @@
 #!/bin/sh
 
 MAXHITS=10
-QUALID=3C2W12C2LGZN4EMQFHW6CYO6T4FRUC #3I79FTN7D40ZECN6E5LO0NRTF2Z4EN
+QUALID=3I79FTN7D40ZECN6E5LO0NRTF2Z4EN #3I79FTN7D40ZECN6E5LO0NRTF2Z4EN
 BLOCKQUALID=3XQ9J27GI6KR2C3EAATQ7K56T9EMHI
 LISTHITS="$(aws mturk list-hits-for-qualification-type  --qualification-type-id $QUALID | jq '.HITs[].HITId')"
 
-# while :
-# do
-WORKERS=()
-for value in $LISTHITS
+while true
 do
-	temp="${value%\"}"
-	temp="${temp#\"}"
-	# echo $temp
-	WORKERIDS="$(aws mturk list-assignments-for-hit --hit-id $temp | jq '.Assignments[].WorkerId')"
-	for worker in $WORKERIDS
+	WORKERS=()
+	for value in $LISTHITS
 	do
-		WORKERS+=($worker)
+		temp="${value%\"}"
+		temp="${temp#\"}"
+		# echo $temp
+		WORKERIDS="$(aws mturk list-assignments-for-hit --hit-id $temp | jq '.Assignments[].WorkerId')"
+		for worker in $WORKERIDS
+		do
+			WORKERS+=($worker)
+		done
+		# echo $(echo ${WORKERS[*]} | tr ' ' '\n' | sort | uniq -c)
 	done
-	# echo $(echo ${WORKERS[*]} | tr ' ' '\n' | sort | uniq -c)
+
+	echo $(echo ${WORKERS[*]} | tr ' ' '\n' | sort | uniq -c)
+	UNIQWORKERS="$(echo ${WORKERS[*]} | tr ' ' '\n' | sort | uniq)"
+	for uniqueworker in $UNIQWORKERS
+	do
+		workercount="$(echo ${WORKERS[*]} | tr ' ' '\n' | grep -c $uniqueworker)"
+		if (($workercount>$MAXHITS))
+		then
+			# aws mturk associate-qualification-with-worker --qualification-type-id $BLOCKQUALID --worker-id $uniqueworker --integer-value 1 --no-send-notification
+			echo $uniqueworker
+		fi
+	done
+
+	sleep 600
 done
-
-# echo ${WORKERS[*]}
-echo $(echo ${WORKERS[*]} | tr ' ' '\n' | sort | uniq -c)
-
-UNIQWORKERS="$(echo ${WORKERS[*]} | tr ' ' '\n' | sort | uniq)"
-
-for uniqueworker in $UNIQWORKERS
-do
-	workercount="$(echo ${WORKERS[*]} | tr ' ' '\n' | grep -c $uniqueworker)"
-	if (($workercount>$MAXHITS))
-	then
-		# aws mturk associate-qualification-with-worker --qualification-type-id $BLOCKQUALID --worker-id $uniqueworker
-		echo $uniqueworker
-	fi
-done
-
-# sleep(600)
-
-# done
