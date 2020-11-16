667ba9e67e2e03f205300934b887407b67f9c94c
wh629 <60375376+wh629@users.noreply.github.com>
2020-11-16 11:01:42 -0500

    in-distribution valname

--- scripts/itereval_and_jiant_commands/summarize-evals-all.sh
+++ scripts/itereval_and_jiant_commands/summarize-evals-all.sh
@@ -16,36 +16,43 @@ declare -A treat2nlidir=( ["baseline"]="1_Baseline_protocol" ["LotS"]="2_Ling_on
 combineds=( 'combined' 'separate' )
 inputs=( 'full' 'hyp' )
 
+cd ${SCRIPT_DIR}
+
 python ${SCRIPT_DIR}/move_best_preds.py
 
 for treatment in "${treatments[@]}"
 do
 	treat_dir=${treat2nlidir[$treatment]}
 
-	# # training data stats
+	# training data stats
 	python ${SCRIPT_DIR}/corpus_stats.py \
 		--verbose \
 		--pushstats \
 		--round ${round} \
 		--fname ${NLI_DATA}/${treat_dir}/train_round${round}_${treatment}.jsonl
 
-	echo ${NLI_DATA}/${treat_dir}/train_round${round}_${treatment}.jsonl
-
 	# predictions
 	if [ $treatment == 'baseline' ]
 	then
-		valname=val_round${round}_base.jsonl
+		valname=val_round${round}_base
 	else
-		valname=val_round${round}_${treatment}.jsonl
+		valname=val_round${round}_${treatment}
 	fi
 
 	for combined in "${combineds[@]}"
 	do
+		if [ $combined == 'combined' ]
+		then
+			valfile=${valname}_combined.jsonl
+		else
+			valfile=${valname}.jsonl
+		fi
+		
 		# in-distribution validations
 		for input in "${inputs[@]}"
 		do
 			python ${SCRIPT_DIR}/summarize_evals.py \
-				${NLI_DATA}/${treat_dir}/${valname} \
+				${NLI_DATA}/${treat_dir}/${valfile} \
 				${PRED_DATA}/${treat_dir}/r${round}/${combined}/${input}/val_preds.p
 		done
 
