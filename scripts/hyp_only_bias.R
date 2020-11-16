fd5c21645fc188a3e12ddeaebd0cf45f73cd52f0
Alicia Parrish <alicia.v.parrish@gmail.com>
2020-11-16 14:55:36 -0500

    extra plots of hyp only bias

--- scripts/hyp_only_bias.R
+++ scripts/hyp_only_bias.R
@@ -7,9 +7,9 @@ set.seed(42)
 round = "round2"
 r = "r2"
 
-base_pred<-read.csv(paste0("../predictions/1_Baseline_protocol/",r,"/combined/full/val_",round,"_base_combined_preds.csv"))
-LotS_pred<-read.csv(paste0("../predictions/2_Ling_on_side_protocol/",r,"/combined/full/val_",round,"_LotS_combined_preds.csv"))
-LitL_pred<-read.csv(paste0("../predictions/3_Ling_in_loop_protocol/",r,"/combined/full/val_",round,"_LitL_combined_preds.csv"))
+base_pred<-read.csv(paste0("../predictions/1_Baseline_protocol/",r,"/combined/hyp/val_",round,"_base_combined_preds.csv"))
+LotS_pred<-read.csv(paste0("../predictions/2_Ling_on_side_protocol/",r,"/combined/hyp/val_",round,"_LotS_combined_preds.csv"))
+LitL_pred<-read.csv(paste0("../predictions/3_Ling_in_loop_protocol/",r,"/combined/hyp/val_",round,"_LitL_combined_preds.csv"))
 
 h_ids = LotS_pred %>% select(heuristic,promptID)
 h_ids2 = LitL_pred %>% select(heuristic,promptID)
@@ -23,7 +23,7 @@ accuracy_by_heuristic<-function(dat,heur){
                              correct=="False" ~ 0))%>%
     {if(!heur) group_by(.,heuristic,label,group,round) else .}%>%
     {if(heur) group_by(.,heuristic,label,group,round,heuristic_gold_label) else .}%>%
-    summarise(accuracy=mean(score),count=n())%>%
+    summarise(accuracy=mean(score),count=n())
   if(!heur){dat3$heuristic_gold_label = "No"}
   return(dat3)
 }
@@ -49,12 +49,10 @@ all_acc4<-all_acc3 %>%
     geom_text(aes(label=count),position=position_dodge(width = 1),size=2,face="bold")+
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
     ylab("Accuracy difference from mean")+
+    ggtitle("Hypothesis only bias after Round 2")+
     facet_grid(heuristic_gold_label ~ group*round ,scales="free_x")
-  )
-
-ggsave(paste0("figures/accuracyHeuristic_",round,"_combined.png"), plot=plt, width = 12, height = 6)
-
-
+)
+ggsave(paste0("figures/HypOnlyaccuracyHeuristic_",round,"_combined.png"), plot=plt, width = 12, height = 6)
 
 ########### DON'T SEPARATE BY INDIVIDUAL HEURISTIC ############
 
@@ -91,8 +89,48 @@ all_accs4<-all_accs3 %>%
     geom_text(aes(label=count),position=position_dodge(width = 1),size=2,face="bold")+
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
     ylab("Accuracy difference from mean")+
+    ggtitle("Hypothesis only bias after Round 2")+
     facet_grid(heuristic_applied ~ group)
 )
 
-ggsave(paste0("figures/accuracyHeuristicApplied_",round,"_combined.png"), plot=plt2, width = 8, height = 4)
+ggsave(paste0("figures/HypOnlyAccuracyHeuristicApplied_",round,"_combined.png"), plot=plt2, width = 8, height = 4)
+
+########### BIAS BY LABEL ############
+
+agg_accuracies2<-function(dat){
+  dat2<-dat%>%
+    mutate(score = case_when(correct=="True" ~ 1,
+                             correct=="False" ~ 0))%>%
+    #group_by(label,group,round)%>%
+    group_by(group,round)%>%
+    summarise(accuracy=mean(score),count=n())
+  return(dat2)
+}
+
+agg_base<-agg_accuracies2(base_pred)
+agg_LotS<-agg_accuracies2(LotS_pred)
+agg_LitL<-agg_accuracies2(LitL_pred)
+
+all_agg<-rbind(agg_base,agg_LotS,agg_LitL)
+
+mean_accs<-all_agg%>%
+  group_by(group,round)%>%
+  summarise(mean_acc = weighted.mean(accuracy,count))
+
+agg_accs3<-merge(all_agg,mean_accs)
+
+(plt3<-ggplot(data=agg_accs3,aes(x=round,y=accuracy,col=label,group=label))+
+    #geom_bar(stat='identity',position=position_dodge2(preserve = "single", padding = 0))+
+    geom_line(aes(y=mean_acc),col="gray45",linetype = "solid",size=1.2)+
+    geom_point(aes(y=mean_acc),col="gray45",size=2)+
+    geom_line(size=1.5)+
+    geom_point(size=2)+
+    #geom_text(aes(label=count),position=position_dodge(width = 1),size=2)+
+    #theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
+    ylab("Accuracy")+
+    ggtitle("Hypothesis only bias after Round 2")+
+    facet_wrap(~group)
+)
+
+ggsave(paste0("figures/HypOnly_byLabel_",round,"_combined.png"), plot=plt3, width = 5.5, height = 3)
 
