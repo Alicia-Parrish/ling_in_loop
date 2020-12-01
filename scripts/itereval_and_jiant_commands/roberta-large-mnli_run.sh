5607a71283bc0e6b370bbffb279ca9b4a11e87ca
wh629 <wh629@nyu.edu>
2020-12-01 13:38:48 -0500

    roberta-large-mnli

---
+++ scripts/itereval_and_jiant_commands/roberta-large-mnli_run.sh
@@ -0,0 +1,90 @@
+sh eval-models.sh baseline_1 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_1 LotS_1 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_1 LitL_1 0.00002 32 roberta-large-mnli
+sh eval-models.sh baseline_1_separate 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_1_separate LotS_1 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_1_separate LitL_1 0.000005 32 roberta-large-mnli
+sh eval-models.sh baseline_2 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_2 LotS_2 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_2 LitL_2 0.000005 32 roberta-large-mnli
+sh eval-models.sh baseline_2_separate 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_2_separate LotS_2 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_2_separate LitL_2 0.00002 32 roberta-large-mnli
+sh eval-models.sh baseline_3 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh baseline_3 LotS_3 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh baseline_3 LitL_3 0.00001 16 roberta-large-mnli
+sh eval-models.sh baseline_3_separate 0.000005 16 roberta-large-mnli
+sh cross-eval-models.sh baseline_3_separate LotS_3 0.000005 16 roberta-large-mnli
+sh cross-eval-models.sh baseline_3_separate LitL_3 0.000005 16 roberta-large-mnli
+sh eval-models.sh baseline_4 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_4 LotS_4 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_4 LitL_4 0.00002 32 roberta-large-mnli
+sh eval-models.sh baseline_4_separate 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_4_separate LotS_4 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_4_separate LitL_4 0.00001 32 roberta-large-mnli
+sh eval-models.sh baseline_5 0.000005 16 roberta-large-mnli
+sh cross-eval-models.sh baseline_5 LotS_5 0.000005 16 roberta-large-mnli
+sh cross-eval-models.sh baseline_5 LitL_5 0.000005 16 roberta-large-mnli
+sh eval-models.sh baseline_5_separate 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_5_separate LotS_5 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh baseline_5_separate LitL_5 0.000005 32 roberta-large-mnli
+sh eval-models.sh LotS_1 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_1 baseline_1 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_1 LitL_1 0.00001 32 roberta-large-mnli
+sh eval-models.sh LotS_1_separate 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_1_separate baseline_1 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_1_separate LitL_1 0.00001 32 roberta-large-mnli
+sh eval-models.sh LotS_2 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_2 baseline_2 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_2 LitL_2 0.000005 32 roberta-large-mnli
+sh eval-models.sh LotS_2_separate 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LotS_2_separate baseline_2 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LotS_2_separate LitL_2 0.00001 16 roberta-large-mnli
+sh eval-models.sh LotS_3 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_3 baseline_3 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_3 LitL_3 0.00001 32 roberta-large-mnli
+sh eval-models.sh LotS_3_separate 0.000005 16 roberta-large-mnli
+sh cross-eval-models.sh LotS_3_separate baseline_3 0.000005 16 roberta-large-mnli
+sh cross-eval-models.sh LotS_3_separate LitL_3 0.000005 16 roberta-large-mnli
+sh eval-models.sh LotS_4 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_4 baseline_4 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_4 LitL_4 0.00002 32 roberta-large-mnli
+sh eval-models.sh LotS_4_separate 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_4_separate baseline_4 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_4_separate LitL_4 0.00001 32 roberta-large-mnli
+sh eval-models.sh LotS_5 0.00003 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_5 baseline_5 0.00003 32 roberta-large-mnli
+sh cross-eval-models.sh LotS_5 LitL_5 0.00003 32 roberta-large-mnli
+sh eval-models.sh LotS_5_separate 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LotS_5_separate baseline_5 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LotS_5_separate LitL_5 0.00001 16 roberta-large-mnli
+sh eval-models.sh LitL_1 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_1 baseline_1 0.00001 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_1 LotS_1 0.00001 32 roberta-large-mnli
+sh eval-models.sh LitL_1_separate 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LitL_1_separate baseline_1 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LitL_1_separate LotS_1 0.00001 16 roberta-large-mnli
+sh eval-models.sh LitL_2 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LitL_2 baseline_2 0.00001 16 roberta-large-mnli
+sh cross-eval-models.sh LitL_2 LotS_2 0.00001 16 roberta-large-mnli
+sh eval-models.sh LitL_2_separate 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_2_separate baseline_2 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_2_separate LotS_2 0.000005 32 roberta-large-mnli
+sh eval-models.sh LitL_3 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_3 baseline_3 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_3 LotS_3 0.000005 32 roberta-large-mnli
+sh eval-models.sh LitL_3_separate 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_3_separate baseline_3 0.00002 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_3_separate LotS_3 0.00002 32 roberta-large-mnli
+sh eval-models.sh LitL_4 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_4 baseline_4 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_4 LotS_4 0.000005 32 roberta-large-mnli
+sh eval-models.sh LitL_4_separate 0.00003 16 roberta-large-mnli
+sh cross-eval-models.sh LitL_4_separate baseline_4 0.00003 16 roberta-large-mnli
+sh cross-eval-models.sh LitL_4_separate LotS_4 0.00003 16 roberta-large-mnli
+sh eval-models.sh LitL_5 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_5 baseline_5 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_5 LotS_5 0.000005 32 roberta-large-mnli
+sh eval-models.sh LitL_5_separate 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_5_separate baseline_5 0.000005 32 roberta-large-mnli
+sh cross-eval-models.sh LitL_5_separate LotS_5 0.000005 32 roberta-large-mnli
