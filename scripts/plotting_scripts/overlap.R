867305df766d6b779e1467bfe00a8d66d31b1628
Alicia Parrish <alicia.v.parrish@gmail.com>
2020-12-01 15:39:19 -0500

    added bias score calculation and plots

--- scripts/plotting_scripts/overlap.R
+++ scripts/plotting_scripts/overlap.R
@@ -64,3 +64,90 @@ ggsave(paste0("figures/CorpusStats/OverlapRate",round,"_combined.png"), plot=plt
 
 ggsave(paste0("figures/CorpusStats/OverlapRate",round,"_byRound.png"), plot=plt2, width = 8, height = 8)
 
+
+#####################################
+# flip color and facet
+(plt_flip = ggplot(all_data,aes(x=X0,fill=group))+
+    geom_bar(position=position_dodge2(preserve = "single"),width=0.75)+
+    scale_x_binned(breaks = seq(0,1,0.1))+
+    #scale_y_continuous(trans = 'log10')+
+    #facet_grid(round~group)
+    facet_grid(~label)+
+    xlab("overlap rate")+
+    #ylab("log10 count")+
+    ggtitle("Overlap rates in each protocol - all rounds combined")+
+    theme(plot.title = element_text(hjust = 0.5))+
+    scale_fill_manual(values=c(cols[3],cols[4],cols[9]))
+)
+
+(plt2_flip = ggplot(all_data,aes(x=X0,col=group))+
+    scale_color_manual(values=c(cols[3],cols[4],cols[9]))+
+    geom_freqpoly(binwidth = 0.1,size=1.2)+
+    #scale_y_continuous(trans = 'log10')+
+    facet_grid(round~label)+
+    xlab("overlap rate")+
+    ggtitle("Overlap rates in each protocol")+
+    theme(plot.title = element_text(hjust = 0.5))
+)
+
+################################
+# calculate bias scores
+
+all_data_bias <- all_data %>%
+  mutate(x_bins = cut(X0, breaks = seq(0,1,0.1), include.lowest = T))%>%
+  group_by(x_bins,group,label)%>%
+  summarise(count=n())%>%
+  ungroup()%>%
+  spread(label,count)
+
+entailment_bias <- all_data_bias%>%
+  mutate(over_N = entailment - neutral,
+         over_C = entailment - contradiction,
+         over_E = NA
+         )%>%
+  mutate(bias = "entailment_bias")
+
+neutral_bias <- all_data_bias%>%
+  mutate(over_E = neutral - entailment,
+         over_C = neutral - contradiction,
+         over_N = NA
+  )%>%
+  mutate(bias = "neutral_bias")
+
+contradiction_bias <- all_data_bias%>%
+  mutate(over_N = contradiction - neutral,
+         over_E = contradiction - entailment,
+         over_C = NA
+  )%>%
+  mutate(bias = "contradiction_bias")
+
+all_biases <- rbind(entailment_bias,neutral_bias,contradiction_bias)
+
+all_biases2<-all_biases%>%
+  select(-entailment,-contradiction,-neutral)%>%
+  gather("comparison","diff_score",-x_bins,-group,-bias)
+
+(plt_bias = ggplot(all_biases2,aes(x=x_bins,y=diff_score,col=comparison,fill=comparison))+
+    geom_bar(stat="identity",position=position_dodge())+
+    theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1))+
+    facet_grid(group~bias)+
+    ggtitle("Overlap rate bias scores")+
+    theme(plot.title = element_text(hjust = 0.5))
+  )
+
+ggsave(paste0("figures/CorpusStats/OverlapRate_biasScore_",round,".png"), plot=plt_bias, width = 10, height = 7)
+
+all_biases3<-all_biases2%>%
+  mutate(full_bias = paste0(bias,"_",comparison))
+
+(plt_bias2 = ggplot(all_biases3,aes(x=x_bins,y=diff_score,fill=group))+
+    geom_bar(stat="identity",position=position_dodge())+
+    theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1))+
+    facet_wrap(~full_bias)+
+    ggtitle("Overlap rate bias scores")+
+    theme(plot.title = element_text(hjust = 0.5))+
+    scale_fill_manual(values=c(cols[3],cols[4],cols[9]))
+)
+
+ggsave(paste0("figures/CorpusStats/OverlapRate_biasScore2_",round,".png"), plot=plt_bias2, width = 10, height = 7)
+
