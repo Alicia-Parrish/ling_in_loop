8f691fac010921c7d88b0231a8635c308f4e3a7c
Alicia Parrish <alicia.v.parrish@gmail.com>
2020-12-01 12:39:58 -0500

    accuracy by annotator confidence

--- scripts/plotting_scripts/accuracy_by_validationConfidence.R
+++ scripts/plotting_scripts/accuracy_by_validationConfidence.R
@@ -4,12 +4,12 @@ library(ggplot2)
 setwd("C:/Users/NYUCM Loaner Access/Documents/GitHub/ling_in_loop/scripts")
 
 set.seed(42)
-round = "round4"
-r = "r4"
+round = "round5"
+r = "r5"
 
-base_pred<-read.csv(paste0("../predictions/1_Baseline_protocol/",r,"/combined/full/val_",round,"_base_combined_preds.csv"))
-LotS_pred<-read.csv(paste0("../predictions/2_Ling_on_side_protocol/",r,"/combined/full/val_",round,"_LotS_combined_preds.csv"))
-LitL_pred<-read.csv(paste0("../predictions/3_Ling_in_loop_protocol/",r,"/combined/full/val_",round,"_LitL_combined_preds.csv"))
+base_pred<-read.csv(paste0("../predictions/roberta-large/1_Baseline_protocol/",r,"/combined/full/val_",round,"_base_combined_preds.csv"))
+LotS_pred<-read.csv(paste0("../predictions/roberta-large/2_Ling_on_side_protocol/",r,"/combined/full/val_",round,"_LotS_combined_preds.csv"))
+LitL_pred<-read.csv(paste0("../predictions/roberta-large/3_Ling_in_loop_protocol/",r,"/combined/full/val_",round,"_LitL_combined_preds.csv"))
 
 accuracy_by_validationConfidence<-function(dat){
   dat$num_label<-NA
@@ -35,9 +35,9 @@ all_preds<-rbind(base_pred2,LotS_pred2,LitL_pred2)
   geom_point(aes(size=count),alpha=0.5)+
   ylab('Accuracy')+
   xlab('Number of annotators agreeing on gold label')+
-  ggtitle('Accuracy by annotator confidence (round 4)')+
+  ggtitle('Accuracy by annotator confidence (round 5)')+
   theme(plot.title = element_text(hjust = 0.5))+
   scale_x_continuous(breaks=c(3,4,5))+
   facet_wrap(~group))
 
-ggsave("figures/acc_by_confidence_round4_combined.png", plot=plt, width = 7, height = 5)
+ggsave("figures/acc_by_confidence_round5_combined.png", plot=plt, width = 7, height = 5)
