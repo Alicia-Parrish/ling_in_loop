636b48db2c15a57cf28ed920d13aff5cace33693
wh629 <wh629@nyu.edu>
2020-12-17 11:41:08 -0500

    to merge

---
+++ scripts/itereval_and_jiant_commands/ad_hoc/temp-eval-cross_eval.sh
@@ -0,0 +1,11 @@
+cd ..
+
+sh roberta-large_sampled_cross_eval_1_run.sh
+sh roberta-large_sampled_cross_eval_2_run.sh
+sh roberta-large_sampled_cross_eval_3_run.sh
+sh roberta-large_sampled_cross_eval_4_run.sh
+
+sh roberta-large-mnli_sampled_cross_eval_1_run.sh
+sh roberta-large-mnli_sampled_cross_eval_2_run.sh
+sh roberta-large-mnli_sampled_cross_eval_3_run.sh
+sh roberta-large-mnli_sampled_cross_eval_4_run.sh
