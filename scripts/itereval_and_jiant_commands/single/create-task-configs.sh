6d5b3ffa96cbd0f315219df110c548ca2d0e16a5
wh629 <wh629@cm07.nyu.cluster>
2021-04-13 21:11:22 -0400

    anli

--- scripts/itereval_and_jiant_commands/single/create-task-configs.sh
+++ scripts/itereval_and_jiant_commands/single/create-task-configs.sh
@@ -7,7 +7,8 @@ BASE_DIR=$PWD
 
 DATA_DIR=${BASE_DIR}/tasks/data/${TASK_NAME}
 ITEREVAL=${BASE_DIR}/tasks/data/iterative_eval/val_itercombined.jsonl
-MNLIEVAL=${BASE_DIR}/tasks/data/mnli_mismatched/val_mnli.jsonl
+MNLIEVAL=${BASE_DIR}/tasks/data/mnli_mismatched/val_mismatched_mnli.jsonl
+ANLIEVAL=${BASE_DIR}/tasks/data/anli_combined/val_anli.jsonl
 MODELS_DIR=${BASE_DIR}/models
 
 export PYTHONPATH=${BASE_DIR}/jiant:$PYTHONPATH
@@ -21,7 +22,8 @@ then
     python jiant/jiant/scripts/preproc/litl/make_task_config.py \
     --data_path ${DATA_DIR} \
     --itereval_path ${ITEREVAL} \
-    --mnlieval_path ${MNLIEVAL}
+    --eval_paths ${MNLIEVAL},${ANLIEVAL} \
+    --eval_names mnlieval,anlieval
 
 elif [ "$HYP" == "true" ] 
 then
@@ -30,6 +32,8 @@ then
     python jiant/jiant/scripts/preproc/litl/make_task_config.py \
     --data_path ${DATA_DIR} \
     --itereval_path ${ITEREVAL} \
-    --mnlieval_path ${MNLIEVAL} \
+    --eval_paths ${MNLIEVAL},${ANLIEVAL} \
+    --eval_names mnlieval,anlieval \
     --hypothesis
 fi
+
